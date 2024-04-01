cd55955c91bc75102b6215fb8f46cc6215eb4dcc
Rudi <rudi@sakuradev.com>
2024-04-01 15:02:18 +0200

    Fix page we are navigating to

--- lib/2_application/pages/spells/spells_information_page.dart
+++ lib/2_application/pages/spells/spells_information_page.dart
@@ -1,27 +1,31 @@
-// this widget should take in a SpellEntity and display the name, type, incantation, effect, canBeVerbal, and creator of the spell.
-// It will not use any cubit or bloc, but will be a simple StatelessWidget that takes in a SpellEntity and displays the information.
-// There must be no scaffold as we will navigate to this page from the existing spells page when the user clicks on a spell.
-
 import 'package:flutter/material.dart';
 import 'package:wizarding_world/1_domain/entities/spell_entities/spell_entity.dart';
 
 class SpellsInformationPage extends StatelessWidget {
   final SpellEntity spell;
 
-  const SpellsInformationPage({Key? key, required this.spell})
-      : super(key: key);
+  const SpellsInformationPage({super.key, required this.spell});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text('Name: ${spell.name}'),
-        Text('Type: ${spell.type}'),
-        Text('Incantation: ${spell.incantation}'),
-        Text('Effect: ${spell.effect}'),
-        Text('Can be verbal? ${spell.canBeVerbal ? 'Yes' : 'No'}'),
-        Text('Creator: ${spell.creator.isEmpty ? 'Unknown' : spell.creator}'),
-      ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(spell.name),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Type: ${spell.type}'),
+            Text('Incantation: ${spell.incantation}'),
+            Text('Effect: ${spell.effect}'),
+            Text('Can be verbal? ${spell.canBeVerbal ? 'Yes' : 'No'}'),
+            Text(
+                'Creator: ${spell.creator.isEmpty ? 'Unknown' : spell.creator}'),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/2_application/pages/spells/spells_page.dart
+++ lib/2_application/pages/spells/spells_page.dart
@@ -4,17 +4,6 @@ import 'package:wizarding_world/2_application/pages/spells/cubit/spells_cubit.da
 import 'package:wizarding_world/2_application/pages/spells/cubit/spells_state.dart';
 import 'package:wizarding_world/2_application/pages/spells/spells_information_page.dart';
 
-// The spells page is a StatelessWidget that uses the SpellsCubit to manage the state of the page.
-// The SpellsCubit is responsible for fetching the spells data from the repository and updating the state of the page.
-// The spells page must be wrapped in a BlocProvider that is generic over SpellsCubit to provide the SpellsCubit to the page.
-// The spells page uses the BlocBuilder widget to rebuild the page whenever the state of the SpellsCubit changes.
-// The spalls page must be able to flip its UI with an animation, on the one side, it must allow the user to specify
-// the name of the spell, the type of the spell (which is a hardcoded list of strings that include None, Charm, Conjuration, etc),
-// along with the incantation for the spell.
-// The user will then press a button, and this button will call spellRequested and give the name of the spell, the type of the spell, and the incantation
-// to the function.
-// The other side of the UI will show a list of spells that are fetched from the GetSpellsUseCase.
-
 class SpellsPage extends StatelessWidget {
   const SpellsPage({Key? key}) : super(key: key);
 
@@ -48,6 +37,15 @@ class SpellsPage extends StatelessWidget {
                           'Creator: ${spell.creator.isEmpty ? 'Unknown' : spell.creator}'),
                     ],
                   ),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) =>
+                            SpellsInformationPage(spell: spell),
+                      ),
+                    );
+                  },
                 );
               },
             );
