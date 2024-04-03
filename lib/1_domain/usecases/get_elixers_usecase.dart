aa007b2a525150964abca7f85784b12d81bd9161
Rudi <rudi@sakuradev.com>
2024-04-03 18:49:14 +0200

    Sort elixirs alphabetically by name by default

--- lib/1_domain/usecases/get_elixers_usecase.dart
+++ lib/1_domain/usecases/get_elixers_usecase.dart
@@ -5,9 +5,24 @@ import 'package:wizarding_world/1_domain/failures/failures.dart';
 import 'package:wizarding_world/1_domain/repositories/elixirs_repository.dart';
 
 class GetElixirsUseCase {
-  final ElixirRepository _elixirsRepository = ElixirRepositoryImpl();
+  final ElixirRepository elixirsRepository = ElixirRepositoryImpl();
 
-  Future<Either<Failure, List<ElixirEntity>>> getElixirs() {
-    return _elixirsRepository.getElixirs();
+  Future<Either<Failure, List<ElixirEntity>>> getElixirs(
+      {bool sortOrderIsAscending = true}) async {
+    final elixirsOrFailure = await elixirsRepository.getElixirs();
+    elixirsOrFailure.fold(
+        (failure) => left(failure),
+        (elixirs) => {
+              elixirs.sort((a, b) {
+                if (sortOrderIsAscending) {
+                  return a.name.compareTo(b.name);
+                } else {
+                  return b.name.compareTo(a.name);
+                }
+              }),
+              right(elixirs)
+            });
+
+    return elixirsOrFailure;
   }
 }
